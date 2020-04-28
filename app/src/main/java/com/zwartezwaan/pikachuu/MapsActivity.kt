2dd1e13b70cd2b20501e650a9883495f2ec1d3d0
Pearl Jean <perlegirl@protonmail.com>
2020-04-28 18:01:30 -0400

    don't know how to make capture work..

--- app/src/main/java/com/zwartezwaan/pikachuu/MapsActivity.kt
+++ app/src/main/java/com/zwartezwaan/pikachuu/MapsActivity.kt
@@ -26,7 +26,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-val mapFragment = supportFragmentManager
+    val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
@@ -34,37 +34,35 @@ val mapFragment = supportFragmentManager
         LoadPoky()
     }
     //for dangerous permission (only needed for ver 23 or later)
-var AccessLocation = 123
+    var LocatieToegang = 123
+
     fun checkPerm(){
         if(Build.VERSION.SDK_INT>=23){
             if(ActivityCompat.checkSelfPermission(
                     this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
                     PackageManager.PERMISSION_GRANTED)
-            {requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), AccessLocation )
+            {requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LocatieToegang )
                 return }
         }
         //otherwise older versions won't require further so just run the below func
-        getUserLocation()
-    }
+        VindGebruikerLocatie()    }
 
-    fun getUserLocation(){
+    fun VindGebruikerLocatie(){
         Toast.makeText(this, "location access on! ", Toast.LENGTH_LONG).show()
-var mijnLocatie = MoiLocLis()
-var locatieManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    var mijnLocatie = MoiLocLis()
+    var locatieManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
          locatieManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3, 3f, mijnLocatie)
-var moiThread= mijnDraad()
+    var moiThread= mijnDraad()
         moiThread.start()
     }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+        grantResults: IntArray ) {
         when(requestCode)
-        {AccessLocation ->
-            {
-             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {getUserLocation()}
+        {LocatieToegang ->
+            { if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {VindGebruikerLocatie()  }
                 else{Toast.makeText(this, "we could not access your location", Toast.LENGTH_LONG).show()  }
             }
         }
@@ -79,39 +77,32 @@ var moiThread= mijnDraad()
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-    }
+    override fun onMapReady(googleMap: GoogleMap) { mMap = googleMap  }
+    var userHealth = 0.0
 
+//list of pokemons .
+var pokemonyList = ArrayList<Pokemony>()
 var locatie:Location?=null
+var oldLocatie:Location?=null
+    //vind gebruiker locatie
 
-    //get user location
     inner class MoiLocLis:LocationListener{
         constructor(){
             locatie= Location("Start")
             locatie!!.latitude= 0.0
             locatie!!.longitude= 0.0 }
 
-        override fun onLocationChanged(p0: Location?) {
-           locatie= p0 }
+        override fun onLocationChanged(p0: Location?) { locatie= p0 }
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {}
+        override fun onProviderEnabled(p0: String?) { }
+        override fun onProviderDisabled(p0: String?) {} }
 
-        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
-            TODO("Not yet implemented") }
-
-        override fun onProviderEnabled(p0: String?) {
-            TODO("Not yet implemented") }
-
-        override fun onProviderDisabled(p0: String?) {
-            TODO("Not yet implemented") }
-    }
-
-var oldLocatie:Location?=null
     inner class mijnDraad:Thread{
+
         constructor():super(){
-            oldLocatie = Location("Start")
-            oldLocatie!!.latitude = 0.0
-            oldLocatie!!.longitude= 0.0
-        }
+        oldLocatie = Location("Start")
+        oldLocatie!!.latitude = 0.0
+        oldLocatie!!.longitude= 0.0  }
 
         override fun run(){
             while(true){
@@ -123,54 +114,52 @@ var oldLocatie:Location?=null
                         mMap!!.clear()
                         // Add a marker in Sydney and move the camera
                     // for displaying the user
-val sydney = LatLng(locatie!!.latitude, locatie!!.longitude)
+                    val mijnLocatie = LatLng(locatie!!.latitude, locatie!!.longitude)
                         mMap!!.addMarker(MarkerOptions()
-                            .position(sydney)
+                            .position(mijnLocatie)
                             .title("Pikachuu")
-                            .snippet("my location")
+                            .snippet("impact: 67")
                             .icon(BitmapDescriptorFactory.fromResource(R.drawable.pikapikachu)))
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 5f))
+                        mMap!!.moveCamera(CameraUpdateFactory.newLatLngZoom(mijnLocatie, 14f))
 
                     //for showing pokemons
                         for(p in 0..pokemonyList.size-1){
-                               var newP = pokemonyList[p]
+
+                            var newP = pokemonyList[p]
                                 if(newP.caught==false){
-                                    var pLocatie = LatLng(newP.lat!!, newP.long!!)
+                                    val pLocatie = LatLng(newP.locatie!!.latitude, newP.locatie!!.longitude)
                                     mMap!!.addMarker(MarkerOptions()
                                         .position(pLocatie)
                                         .title(newP.name!!)
-                                        .snippet(newP.desc!!)
-                                        .icon(BitmapDescriptorFactory.fromResource(newP.img!!))
-                                    )
-                                }
-                        }
-                    }
-                    Thread.sleep(1000)
-                }catch(ex:Exception){}
-            }
-        }
-    }
+                                        .snippet("impact: ${newP.impact!!}")
+                                        .icon(BitmapDescriptorFactory.fromResource(newP.img!!)))
+                                if (locatie!!.distanceTo(newP.locatie) < 2 ){
+                                        newP.caught = true
+                                        pokemonyList[p] = newP
+                                        userHealth += newP.impact!!
+                                         Toast.makeText(applicationContext,
+                                        "Acquired ${newP.name!!}. Your health is upgraded to $userHealth",
+                                        Toast.LENGTH_LONG).show() }
+                                } } }
+                    Thread.sleep(1000) }catch(ex:Exception){} } } } //end of mijnDraad
 
-    //list of pokemons from pokemony.kt
-var pokemonyList = ArrayList<Pokemony>()
-    //creates list of pokemons
-    fun LoadPoky(){
-        pokemonyList.add( Pokemony(R.drawable.kirby, "Kirbyy", "Kirrbyyy",
-            30.0, 44.8737, -0.5546)  )
-        pokemonyList.add( Pokemony(R.drawable.itachii, "Itachi",
-            "The Hero We Do Not Deserve", 70.0, 34.4682, 135.5320)  )
-        pokemonyList.add( Pokemony(R.drawable.peach,
-            "Peach", "Princess Peach", 40.0, 59.2325, 18.0760)  )
-        pokemonyList.add( Pokemony(R.drawable.assassin,
-            "Assassin", "My DNA is the Future", 88.0,
-            31.6034, 34.8710)  )
-        pokemonyList.add( Pokemony(R.drawable.zelda,
-            "Princess Zelda", "Elf Princess of the Forest", 68.0, 44.4525, 26.0608)  )
-        pokemonyList.add( Pokemony(R.drawable.kyloren,
-            "Kylo Ren", "Prince of Darkness", 78.0, 62.0410, 73.3467)  )
-        pokemonyList.add( Pokemony(R.drawable.joker,
-            "The Joker", "Why So Seriouss?? ", 55.0, 40.8198, -73.8576)  )
-    }
+//creates list of pokemons
+fun LoadPoky(){
+    pokemonyList.add( Pokemony(R.drawable.kirby, "Kirbyy",
+        "Kirrbyyy", 30.0, -13.5320, -71.9675)  )
+    pokemonyList.add( Pokemony(R.drawable.itachii, "Itachi",
+        "The Hero We Do Not Deserve", 70.0, 35.6804, 139.7690)  )
+    pokemonyList.add( Pokemony(R.drawable.peach,
+        "Peach", "Princess Peach", 40.0, 34.6937, 135.5023)  )
+    pokemonyList.add( Pokemony(R.drawable.assassin,
+        "Assassin", "My DNA is the Future", 88.0, 33.5138, 36.2765)  )
+    pokemonyList.add( Pokemony(R.drawable.zelda,
+        "Princess Zelda", "Elf Princess of the Forest", 68.0, 59.3293, 18.0686)  )
+    pokemonyList.add( Pokemony(R.drawable.kyloren,
+        "Kylo Ren", "Prince of Darkness", 78.0, 58.0092, 56.2270)  )
+    pokemonyList.add( Pokemony(R.drawable.joker,
+        "The Joker", "Why So Seriouss?? ", 55.0, 40.8448, -73.8648)  )
+}
 
 
 }
--- app/src/main/java/com/zwartezwaan/pikachuu/Pokemony.kt
+++ app/src/main/java/com/zwartezwaan/pikachuu/Pokemony.kt
@@ -1,21 +1,22 @@
 package com.zwartezwaan.pikachuu
+import android.location.Location
 
 class Pokemony {
     var name:String?=null
     var desc:String?=null
     var img:Int?= null
     var impact:Double?= null
-    var lat:Double?=null
-    var long:Double?=null
     var caught:Boolean?=false
+    var locatie:Location?=null
 
     constructor(imgy:Int, namey:String, descc:String, impact:Double, laty:Double, longy:Double){
         this.img = imgy
         this.name = namey
         this.desc = descc
         this.impact = impact
-        this.lat = laty
-        this.long = longy
+        this.locatie = Location(name)
+        this.locatie!!.latitude = laty
+        this.locatie!!.longitude = longy
         this.caught = false
     }
 }
